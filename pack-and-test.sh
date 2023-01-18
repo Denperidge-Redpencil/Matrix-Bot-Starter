71df30ea929ff5bd8fde9ccd88a8aff36c58e462
Denperidge <denperidge@gmail.com>
2023-01-18 12:09:09 +0100

    Added pack-and-test.sh

---
+++ pack-and-test.sh
@@ -0,0 +1,6 @@
+git clone https://github.com/Denperidge-Redpencil/Matrix-Bot-Starter-Starter.git
+npm pack
+cd Matrix-Bot-Starter-Starter/
+npm install ../matrix-bot-starter-*.tgz
+npm run dev
+cd ..
